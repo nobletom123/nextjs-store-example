34a74f5091f9689cf75a4fa9603e608f8ce11d74
Tom <ttbnoble@gmail.com>
2021-10-28 17:18:51 +0100

    added to read me

--- src/client/contexts/profile-context.tsx
+++ src/client/contexts/profile-context.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext } from "react";
-import { ProductType } from "../types/product";
+import { ProductType } from "../../types/product";
 
 type ProfileDataType = {
   user: any;
